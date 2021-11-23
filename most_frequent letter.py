b7540b99e5cac6357f38829f4840728999fac860
MahaLakshmi0411 <94038154+MahaLakshmi0411@users.noreply.github.com>
2021-11-23 19:14:30 +0530

    The file prints most frequent letter in astring

---
+++ most_frequent letter.py
@@ -0,0 +1,11 @@
+x= input('Please enter a string ')
+def i(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+print (i(x))
